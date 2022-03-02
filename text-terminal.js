ea55b2bb2785a45bf5b33de83081b04e9640da75
Ruby Simpleman <rubysimpleman@gmail.com>
2022-03-02 12:21:57 +0200

    2022-03-02_12-21-57_2ways01

--- text-terminal.js
+++ text-terminal.js
@@ -136,24 +136,6 @@ const trinityArr = arrOfDigits.map( (element, index) => {
   }
 })
 
-//const myUsers = [
-//    { name: 'shark', likes: 'ocean' },
-//    { name: 'turtle', likes: 'pond' },
-//    { name: 'otter', likes: 'fish biscuits' }
-//]
-
-//const usersByLikes = myUsers.map(item => {
-//    const container = {};
-
-//    container[item.name] = item.likes;
-//    container.age = item.name.length * 10;
-
-//    return container;
-//})
-
-//console.log(usersByLikes);
-
-
 
 stringteger(103)
 stringteger(204)
